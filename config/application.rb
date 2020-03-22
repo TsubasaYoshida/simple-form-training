0a4035c294d94c084cad06979b31c466b48c5910
y_tsubasa <tbs_chako@yahoo.co.jp>
2020-03-22 12:04:53 +0900

    ジェネレータのカスタマイズを誤っていたため、修正する

--- config/application.rb
+++ config/application.rb
@@ -29,22 +29,24 @@ module SimpleFormTraining
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
-    g.assets false
-    g.helper false
-    g.jbuilder false
-    g.test_framework :rspec,
-                     # テストデータベースにレコードを作成するファイルの作成をスキップ
-                     fixtures: false,
-                     # ビュースペックの作成をスキップ(UIのテストはフィーチャスペックに任せる)
-                     view_specs: false,
-                     # ヘルパースペックの作成をスキップ
-                     helper_specs: false,
-                     # ルーティングスペックの作成をスキップ
-                     routing_specs: false,
-                     # コントローラースペックの作成をスキップ
-                     controller_specs: false,
-                     # リクエストスペックの作成をスキップ
-                     request_specs: false
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.jbuilder false
+      g.test_framework :rspec,
+                       # テストデータベースにレコードを作成するファイルの作成をスキップ
+                       fixtures: false,
+                       # ビュースペックの作成をスキップ(UIのテストはフィーチャスペックに任せる)
+                       view_specs: false,
+                       # ヘルパースペックの作成をスキップ
+                       helper_specs: false,
+                       # ルーティングスペックの作成をスキップ
+                       routing_specs: false,
+                       # コントローラースペックの作成をスキップ
+                       controller_specs: false,
+                       # リクエストスペックの作成をスキップ
+                       request_specs: false
+    end
 
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
